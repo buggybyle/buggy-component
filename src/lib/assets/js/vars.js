a40f720db798c41420e45365edf83a68f600a5cf
Lé <elcasaboxi@gmail.com>
2024-07-07 08:06:15 +0800

    vars twix

--- src/lib/assets/js/vars.js
+++ src/lib/assets/js/vars.js
@@ -1,4 +1,13 @@
-export const API_ENDPOINT = `http://localhost:3001/`;
 export const API_KEY = `component`;
-export const FALLBACK_USER_IMAGE = `https://ollesvelke.vercel.app/images/fallback/user_image.png`;
-export const URL = `http://localhost:3000`;
+
+// prod
+
+// export const API_ENDPOINT = `https://your-api.herokuapp.com/`;
+// export const FALLBACK_USER_IMAGE = `https://your-website.name/images/fallback/user_image.png`;
+// export const URL = `https://your-website.name`;
+
+// dev
+
+export const API_ENDPOINT = `http://localhost:3015/`;
+export const FALLBACK_USER_IMAGE = `https://your-website.name/images/fallback/user_image.png`;
+export const URL = `http://localhost:3014`;
