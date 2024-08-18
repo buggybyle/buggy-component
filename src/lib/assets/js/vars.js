d67352fd888abe2af83e71b335e6bbe4f21972d4
Lé <elcasaboxi@gmail.com>
2024-08-19 04:22:41 +0800

    update vars

--- src/lib/assets/js/vars.js
+++ src/lib/assets/js/vars.js
@@ -1,13 +1,12 @@
 export const API_KEY = `component`;
+export const FALLBACK_USER_IMAGE = `https://buggy.so/images/fallback/user_image.png`;
 
 // prod
 
-// export const API_ENDPOINT = `https://your-api.herokuapp.com/`;
-// export const FALLBACK_USER_IMAGE = `https://your-website.name/images/fallback/user_image.png`;
-// export const URL = `https://your-website.name`;
+export const API_ENDPOINT = `https://buggybyle-142b8554339a.herokuapp.com/`;
+export const URL = `https://buggy.so`;
 
 // dev
 
-export const API_ENDPOINT = `http://localhost:3015/`;
-export const FALLBACK_USER_IMAGE = `https://your-website.name/images/fallback/user_image.png`;
-export const URL = `http://localhost:3014`;
+// export const API_ENDPOINT = `http://localhost:3015/`;
+// export const URL = `http://localhost:3014`;
