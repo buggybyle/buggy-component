d65fd24855c9191d447f3825b6795ae3dbab7d16
lé <elcasaboxi@gmail.com>
2024-09-02 07:09:06 +0700

    update ars

--- src/lib/assets/js/vars.js
+++ src/lib/assets/js/vars.js
@@ -3,10 +3,10 @@ export const FALLBACK_USER_IMAGE = `https://buggy.so/images/fallback/user_image.
 
 // prod
 
-// export const API_ENDPOINT = `https://buggybyle-142b8554339a.herokuapp.com/`;
-// export const URL = `https://buggy.so`;
+export const API_ENDPOINT = `https://buggybyle-142b8554339a.herokuapp.com/`;
+export const URL = `https://buggy.so`;
 
 // dev
 
-export const API_ENDPOINT = `http://localhost:3015/`;
-export const URL = `http://localhost:3014`;
+// export const API_ENDPOINT = `http://localhost:3015/`;
+// export const URL = `http://localhost:3014`;
